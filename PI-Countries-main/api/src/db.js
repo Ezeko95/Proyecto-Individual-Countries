c4fee01e862815d8e375ee43b71cbbd8736bca4c
Ezeko95 <ciangianluca@hotmail.com>
2023-05-19 17:21:24 -0300

    db changes

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -4,15 +4,21 @@ const ActivityModel = require("./models/Activities");
 const fs = require("fs");
 const path = require("path");
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, URL } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(URL, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
